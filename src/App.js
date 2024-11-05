d7618ff42e94ed9b7c7ddb8000737249327e2370
Himanshusystem123 <167021207+Himanshusystem123@users.noreply.github.com>
2024-11-05 10:09:19 +0530

    calculator-app reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
 
 function App() {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState(null);
+
+  const handleAddition = () => setResult(parseFloat(num1) + parseFloat(num2));
+  const handleSubtraction = () => setResult(parseFloat(num1) - parseFloat(num2));
+  const handleMultiplication = () => setResult(parseFloat(num1) * parseFloat(num2));
+  const handleDivision = () => {
+    if (num2 !== '0') setResult(parseFloat(num1) / parseFloat(num2));
+    else setResult('Cannot divide by zero');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Simple Calculator By himanshu </h1>
+      <h2>I am React developer</h2>
+      <input
+        type="number"
+        value={num1}
+        onChange={(e) => setNum1(e.target.value)}
+        placeholder="Enter first number"
+      />
+      <input
+        type="number"
+        value={num2}
+        onChange={(e) => setNum2(e.target.value)}
+        placeholder="Enter second number"
+      />
+      <div style={{ marginTop: '20px' }}>
+        <button onClick={handleAddition}>+</button>
+        <button onClick={handleSubtraction}>-</button>
+        <button onClick={handleMultiplication}>*</button>
+        <button onClick={handleDivision}>/</button>
+      </div>
+      {result !== null && (
+        <h2 style={{ marginTop: '20px' }}>Result: {result}</h2>
+      )}
     </div>
   );
 }
 
 export default App;
+
